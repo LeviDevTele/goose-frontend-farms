d7b34d042d2fc0c91b06a82bf1a39c905120ab63
Levi Dev <86292578+LeviDevTele@users.noreply.github.com>
2021-07-21 19:30:39 +0530

    Update TwitterCard.tsx

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'cash_onyx'
           }}
           options={{
             height: '300',
